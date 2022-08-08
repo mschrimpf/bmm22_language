f418d5dd34ea74dfea185e84d31dcd322825f5af
Martin Schrimpf <m4rtinsch@gmail.com>
2022-08-08 15:17:14 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "brainscore_language @ git+https://github.com/brain-score/language",
+    "transformers",
+    "torch",
+    "datasets",
+    "jupyter",
+    "matplotlib",
+]
+
+setup(
+    name='bmm22language',
+    author="Martin Schrimpf",
+    author_email='msch@mit.edu',
+    url='https://github.com/mschrimpf/bmm22_language',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+)
